70e1b190c1a321743c248e0e6228dd0fb78b0a97
Neno Stefanov <neno.stefanov@miracl.com>
2025-07-15 09:44:43 +0300

    Deploy API documentation to Pages

--- Package.swift
+++ Package.swift
@@ -39,6 +39,7 @@ let package = Package(
         )
     ],
     dependencies: [
+        .package(url: "https://github.com/swiftlang/swift-docc-plugin", from: "1.4.5"),
     ],
     targets: [
         .target(
